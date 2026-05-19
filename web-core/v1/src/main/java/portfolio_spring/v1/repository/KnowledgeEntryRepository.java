01622d0aab246c78505d98e6550395b548fdbf1b
Nahuel7978 <roman.n7978@gmail.com>
2026-05-19 15:59:05 -0300

    Se agrega el método abstracto findById

--- web-core/v1/src/main/java/portfolio_spring/v1/repository/KnowledgeEntryRepository.java
+++ web-core/v1/src/main/java/portfolio_spring/v1/repository/KnowledgeEntryRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import portfolio_spring.v1.model.KnowledgeEntry;
 import java.util.List;
+import java.util.Optional;
 
 public interface KnowledgeEntryRepository extends JpaRepository<KnowledgeEntry, Integer> {
 
     @EntityGraph(attributePaths = {"entryTechnologies.technology"})
     List<KnowledgeEntry> findAll();
+
+    Optional<KnowledgeEntry> findById(int id);
 }
