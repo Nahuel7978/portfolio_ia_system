e41bca9fcc38a9b725b2e0054f0b6275d747859f
Nahuel7978 <roman.n7978@gmail.com>
2026-05-19 17:36:43 -0300

    Se agrega asincronismo

--- web-core/v1/src/main/java/portfolio_spring/v1/V1Application.java
+++ web-core/v1/src/main/java/portfolio_spring/v1/V1Application.java
@@ -2,8 +2,10 @@ package portfolio_spring.v1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class V1Application {
 
 	public static void main(String[] args) {
