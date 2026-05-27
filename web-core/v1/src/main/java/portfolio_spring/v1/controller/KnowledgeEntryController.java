862af8ec11fae76950c9656e9236b2ef56ead94b
Nahuel7978 <roman.n7978@gmail.com>
2026-05-27 10:54:09 -0300

    Se modifica el create para recibir los parametros por form_data, ya que ahora se maneja una imagen. Se implementa un metodo para cambiar la imagen de portada

--- web-core/v1/src/main/java/portfolio_spring/v1/controller/KnowledgeEntryController.java
+++ web-core/v1/src/main/java/portfolio_spring/v1/controller/KnowledgeEntryController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.*;
 import portfolio_spring.v1.dto.KnowledgeEntryDTO;
 import portfolio_spring.v1.service.KnowledgeEntryService;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -32,15 +35,17 @@ public class KnowledgeEntryController {
         }
     }
 
-    @PostMapping
-    public ResponseEntity<?> create(@RequestBody KnowledgeEntryDTO dto) {
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> createEntry(
+            @RequestPart("data") KnowledgeEntryDTO data,
+            @RequestPart(value = "image", required = false) MultipartFile image) {
         try {
-            KnowledgeEntryDTO created = entryService.createEntry(dto);
-            return ResponseEntity.status(HttpStatus.CREATED).body(created);
+            KnowledgeEntryDTO createdEntry = entryService.createEntry(data, image);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdEntry);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(e.getMessage());
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error procesando la imagen");
         }
     }
 
@@ -55,6 +60,33 @@ public class KnowledgeEntryController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
+
+    @PutMapping(value = "/{id}/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> updateCoverImage(
+            @PathVariable("id") Integer id,
+            @RequestPart("image") MultipartFile image) {
+        try {
+        	entryService.updateCoverImage(id, image);
+            return ResponseEntity.ok("Imagen actualizada correctamente.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(e.getMessage());
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error procesando la nueva imagen.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}/image")
+    public ResponseEntity<?> deleteCoverImage(@PathVariable("id") Integer id) {
+        try {
+        	entryService.deleteCoverImage(id);
+            return ResponseEntity.noContent().build(); // Retorna 204 No Content
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id) {
