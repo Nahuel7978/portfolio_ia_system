57a9714444fe72f5fdb1661612d60ae5bf4df353
Nahuel7978 <roman.n7978@gmail.com>
2026-05-27 10:57:18 -0300

    Se agrega toEntity para pasar de un DTO a un Entity

--- web-core/v1/src/main/java/portfolio_spring/v1/mapper/KnowledgeEntryMapper.java
+++ web-core/v1/src/main/java/portfolio_spring/v1/mapper/KnowledgeEntryMapper.java
@@ -4,7 +4,12 @@ import org.springframework.stereotype.Component;
 import portfolio_spring.v1.dto.KnowledgeEntryDTO;
 import portfolio_spring.v1.dto.TechnologyRoleDTO;
 import portfolio_spring.v1.model.KnowledgeEntry;
+import portfolio_spring.v1.model.KnowledgeEntryTechnology;
+import portfolio_spring.v1.model.Technology;
+import portfolio_spring.v1.service.TechnologyService;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +33,44 @@ public class KnowledgeEntryMapper {
                     )).collect(Collectors.toList());
             dto.setTechnologies(techDTOs);
         }
+
+        dto.setHasCoverImage(entry.getImageData() != null);
         return dto;
     }
+
+	public static KnowledgeEntry toEntity(KnowledgeEntryDTO dto) {
+		// TODO Auto-generated method stub
+		if (dto == null) {
+	        return null;
+	    }
+
+		KnowledgeEntry ke = new KnowledgeEntry();
+		if (dto.getId() != null) {
+	        ke.setId(dto.getId());
+	    }
+		ke.setName(dto.getName());
+		ke.setArea(dto.getArea());
+		ke.setAreaSecondary(dto.getAreaSecondary());
+		ke.setStatus(dto.getStatus());
+
+		List<TechnologyRoleDTO> listTec = dto.getTechnologies();
+	    if (listTec != null && !listTec.isEmpty()) {
+			List<KnowledgeEntryTechnology> tecnologies = new ArrayList<KnowledgeEntryTechnology>();
+			for (TechnologyRoleDTO tecDTO : listTec) {
+				KnowledgeEntryTechnology ket = new KnowledgeEntryTechnology();
+				Technology tech = new Technology();
+
+				ket.setRole(tecDTO.getRole());
+				tech.setId(tecDTO.getTechnologyId());
+				ket.setTechnology(tech);
+
+				ket.setEntry(ke);//Vinculamos la relación bidireccional
+
+				tecnologies.add(ket);
+			}
+			ke.setEntryTechnologies(tecnologies);
+		}
+
+		return ke;
+	}
 }
