a4293155b2b3475d530ebdae210f5c07cbfbfdeb
Nahuel7978 <roman.n7978@gmail.com>
2026-05-27 11:02:49 -0300

    Se añade la posibilidad de scheduling

--- web-core/v1/src/main/java/portfolio_spring/v1/V1Application.java
+++ web-core/v1/src/main/java/portfolio_spring/v1/V1Application.java
@@ -1,11 +1,14 @@
 package portfolio_spring.v1;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class V1Application {
 
 	public static void main(String[] args) {
