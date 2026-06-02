1aadb22df430b64bc690f4495dca0e876d43a47c
Nahuel7978 <roman.n7978@gmail.com>
2026-06-02 09:19:46 -0300

    Servicio para conectarse con los endpoints de Knowledge entry

---
+++ frontend/src/app/core/services/knowledge-entry.ts
@@ -0,0 +1,25 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { KnowledgeEntryDTO } from '../models/knowledge-entry.interface';
+
+@Injectable({ providedIn: 'root' })
+export class KnowledgeEntryService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/public/knowledge-entries`;
+
+  getProjects(): Observable<KnowledgeEntryDTO[]> {
+    return this.http.get<KnowledgeEntryDTO[]>(`${this.apiUrl}/projects`);
+  }
+
+  getBlogs(): Observable<KnowledgeEntryDTO[]> {
+    return this.http.get<KnowledgeEntryDTO[]>(`${this.apiUrl}/blogs`);
+  }
+
+  getEntryById(id: number): Observable<any> {
+    // Retorna el PublicEntryDetailDTO que definimos en el backend
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+}
