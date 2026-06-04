ec8d7b12ec2c7d854a8a526436606c358ab13091
Nahuel7978 <roman.n7978@gmail.com>
2026-06-04 18:56:26 -0300

    Se agrega ruta de edicion

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,7 +10,10 @@ export const routes: Routes = [
     canActivate: [authGuard], // <-- Guardián protegiendo todo el módulo admin
     children: [
       { path: 'dashboard', loadComponent: () => import('./features/admin/dashboard/dashboard').then(m => m.DashboardComponent) },
-      { path: 'technologies', loadComponent: () => import('./features/admin/technology/technology').then(m => m.TechnologiesComponent)}
+      { path: 'technologies', loadComponent: () => import('./features/admin/technology/technology').then(m => m.TechnologiesComponent)},
+      { path: 'knowledge-entries', loadComponent: () => import('./features/admin/knowledge-entries/knowledge-entries').then(m => m.KnowledgeEntriesComponent)},
+      { path: 'new-entry', loadComponent: () => import('./features/admin/entries-form/entries-form').then(m => m.EntryFormComponent)},
+      { path: 'edit-entry/:id', loadComponent: () => import('./features/admin/edit-entry/edit-entry').then(m => m.EditEntryComponent)}
     ]
   },
   { path: '**', redirectTo: '' }
