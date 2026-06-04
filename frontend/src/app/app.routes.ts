f1040e3c62ae2f8100470eb0cb09adc5a035cd9e
Nahuel7978 <roman.n7978@gmail.com>
2026-06-04 09:20:10 -0300

    Se agrega la ruta del login y de la sección de tecnologias

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./features/home/home').then(m => m.HomeComponent) },
   { path: 'entry/:id', loadComponent: () => import('./features/detail/detail').then(m => m.DetailComponent) },
+  { path: 'login', loadComponent: () => import('./features/admin/login/login').then(m => m.LoginComponent) },
+  { 
+    path: 'admin', 
+    canActivate: [authGuard], // <-- Guardián protegiendo todo el módulo admin
+    children: [
+      { path: 'dashboard', loadComponent: () => import('./features/admin/dashboard/dashboard').then(m => m.DashboardComponent) },
+      { path: 'technologies', loadComponent: () => import('./features/admin/technology/technology').then(m => m.TechnologiesComponent)}
+    ]
+  },
   { path: '**', redirectTo: '' }
 ];
