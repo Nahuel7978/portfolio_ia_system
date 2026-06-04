dce49400da8ea7e9f343c1ed63a868c1eed986e9
Nahuel7978 <roman.n7978@gmail.com>
2026-06-04 09:22:48 -0300

    Maneja la comunicación con los endpoints para el manejo de las tecnologias

---
+++ frontend/src/app/core/services/technology.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { Technology } from '../models/technology.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TechnologyService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/admin/technologies`;
+
+  getTechnologies(): Observable<Technology[]> {
+    return this.http.get<Technology[]>(this.apiUrl);
+  }
+
+  createTechnology(technology: Technology): Observable<Technology> {
+    return this.http.post<Technology>(this.apiUrl, technology);
+  }
+}
