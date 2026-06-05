0dab7e53608498a4f4d47a0453b3e4a4dcd9dc47
Nahuel7978 <roman.n7978@gmail.com>
2026-06-05 10:48:25 -0300

    Se importa el HeaderComponent

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./features/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
