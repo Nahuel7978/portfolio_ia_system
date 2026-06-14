3348e502ac1fa6c809b5361d5d5071452e3ac054
Nahuel7978 <roman.n7978@gmail.com>
2026-06-14 10:04:00 -0300

    Se quita en DashboardComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,7 +9,6 @@ export const routes: Routes = [
     path: 'admin', 
     canActivate: [authGuard], // <-- Guardián protegiendo todo el módulo admin
     children: [
-      { path: 'dashboard', loadComponent: () => import('./features/admin/dashboard/dashboard').then(m => m.DashboardComponent) },
       { path: 'technologies', loadComponent: () => import('./features/admin/technology/technology').then(m => m.TechnologiesComponent)},
       { path: 'knowledge-entries', loadComponent: () => import('./features/admin/knowledge-entries/knowledge-entries').then(m => m.KnowledgeEntriesComponent)},
       { path: 'new-entry', loadComponent: () => import('./features/admin/entries-form/entries-form').then(m => m.EntryFormComponent)},
