4e73ced99a6b2d53fe87d28474a7ac17531addd6
Nahuel7978 <roman.n7978@gmail.com>
2026-06-16 09:44:40 -0300

    SP5-06: Se agrega dirección de API del bot

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1'
+  apiUrl: 'http://localhost:8080/api/v1',
+  iaApiUrl: 'http://localhost:8001/CV_BOT_API/v1'
 };
