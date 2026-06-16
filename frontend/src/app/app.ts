46dee33c4b50046fcb848b1924b6804d6032267a
Nahuel7978 <roman.n7978@gmail.com>
2026-06-16 09:44:21 -0300

    SP5-06: Se agrega el componente de chat

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./features/header/header";
+import { ChatWidgetComponent } from "./shared/components/chat-widget/chat-widget";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, ChatWidgetComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
