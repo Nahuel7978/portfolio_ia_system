356052ae4dd693aa8b8918c18671803179bc02bc
Nahuel7978 <roman.n7978@gmail.com>
2026-06-23 11:23:31 -0300

    SP6-02: Se agrega manejo de rutas dinamicas con blocking & server

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,7 +2,35 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'admin/technologies',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'admin/knowledge-entries',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'admin/new-entry',
+    renderMode: RenderMode.Prerender,
+  },
+  //Dynamic
+  { path: 'entry/:id',
+    renderMode: RenderMode.Server,
+  },
+  { path: 'admin/edit-entry/:id',
+    renderMode: RenderMode.Server,
+  },
+  // Fallback route
+  {
+    path: '**',
+    renderMode: RenderMode.Server,
   },
 ];
