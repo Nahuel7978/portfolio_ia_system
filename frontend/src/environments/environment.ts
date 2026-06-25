f753944e69e0ad881c18e493a02f832d312b7abc
Nahuel7978 <roman.n7978@gmail.com>
2026-06-25 16:38:36 -0300

    SP6: cambio en prefix

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'http://173.249.28.153/api/v1',
-    iaApiUrl: 'http://173.249.28.153/CV_BOT_API/v1'
+    iaApiUrl: 'http://173.249.28.153/api/chat'
   };
